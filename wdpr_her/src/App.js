b1a97e78a9dc8f58095217676bc541552e1b6605
Brian <17700667+KeijiTadashi@users.noreply.github.com>
2023-11-23 17:51:22 +0100

    Logo test

--- wdpr_her/src/App.js
+++ wdpr_her/src/App.js
@@ -1,4 +1,8 @@
-import logo from './logo.svg';
+import logo from './Logo Icon/icon_accessibility.png';
+import logo_blauw from './Logo Icon/Op blauw/icon_accessibility_on-blue.png';
+import logo_blauw_trans from './Logo Icon/Op blauw/Transparant/icon_accessibility_on-blue_transp.png';
+import logo_donker from './Logo Icon/Op donker/icon_accessibility_on-dark.jpg';
+import logo_donker_trans from './Logo Icon/Op donker/Transparant/icon_accessibility_on-dark_transp.png';
 import './App.css';
 
 function App() {
@@ -6,7 +10,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         Welkom bij Stichting Accessibility
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Logo-row">
+          <p className="Logo-text"><img src={logo} className="App-logo" alt="logo" /> default logo</p>
+          <p className="Logo-text"><img src={logo_blauw} className="App-logo"/> logo on blue</p>
+          <p className="Logo-text"><img src={logo_blauw_trans} className="App-logo"/> logo on blue transparent</p>
+          <p className="Logo-text"><img src={logo_donker} className="App-logo"/> logo on dark </p>
+          <p className="Logo-text"><img src={logo_donker_trans} className="App-logo"/> logo on dark transparent</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
