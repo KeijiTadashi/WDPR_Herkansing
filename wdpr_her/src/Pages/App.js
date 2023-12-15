8a46604ad08ca28035e7bfd3d274ceae50fa3d62
Brian <17700667+KeijiTadashi@users.noreply.github.com>
2023-12-15 13:26:31 +0100

    [Add] header (css)

--- wdpr_her/src/Pages/App.js
+++ wdpr_her/src/Pages/App.js
@@ -1,11 +1,13 @@
-import logo from './Logo Icon/icon_accessibility.png';
-import logo_blauw from './Logo Icon/Op blauw/icon_accessibility_on-blue.png';
-import logo_blauw_trans from './Logo Icon/Op blauw/Transparant/icon_accessibility_on-blue_transp.png';
-import logo_donker from './Logo Icon/Op donker/icon_accessibility_on-dark.jpg';
-import logo_donker_trans from './Logo Icon/Op donker/Transparant/icon_accessibility_on-dark_transp.png';
+import logo from '../Logo Icon/icon_accessibility.png';
+import logo_blauw from '../Logo Icon/Op blauw/icon_accessibility_on-blue.png';
+import logo_blauw_trans from '../Logo Icon/Op blauw/Transparant/icon_accessibility_on-blue_transp.png';
+import logo_donker from '../Logo Icon/Op donker/icon_accessibility_on-dark.jpg';
+import logo_donker_trans from '../Logo Icon/Op donker/Transparant/icon_accessibility_on-dark_transp.png';
 // import './App.css';
-import './Theme.css';
+import '../Theme.css';
 import useLocalStorage from 'use-local-storage';
+import {useAuth0} from "@auth0/auth0-react";
+import {Link} from "react-router-dom";
 
 function App() {
   // Check browser default theme preference
@@ -18,11 +20,19 @@ function App() {
     setTheme(newTheme);
     curThemeLogo = theme === 'light' ? logo : theme === 'blue' ? logo_blauw_trans : logo_donker_trans;
   }
+
+  const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  };
   
   return (
     <div className="App" data-theme={theme}>
       <header className="App-header">
         Welkom bij Stichting Accessibility
+
+        <Link to={"/font"}><h1>GO TO FONT PAGE, HAS THE HEADER AND CORRECT CSS FILE</h1><button>CLICK</button></Link>
         <div className="Logo-row">
           <p className="Logo-text"><img src={logo} className="App-logo" alt="logo" /> default logo</p>
           <p className="Logo-text"><img src={logo_blauw} className="App-logo"/> logo on blue</p>
@@ -34,6 +44,9 @@ function App() {
           <img src={curThemeLogo} className={"App-logo"} alt={"logo stichting accessibility"} /> <button onClick={switchTheme}>
           switch to {theme === 'light' ? 'blue' : theme === 'blue' ? 'dark' : 'light'} theme
         </button></div>
+        
+        <p className={"text-sec"}>Secondary text</p>
+        <p className={"text-accent"}>Text accent</p>
       </header>
     </div>
   );
