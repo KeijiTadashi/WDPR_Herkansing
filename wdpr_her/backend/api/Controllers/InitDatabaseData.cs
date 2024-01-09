d4276c81f3da81cb0d3ea3f7c06287f877b25da1
Brian <17700667+KeijiTadashi@users.noreply.github.com>
2024-01-09 18:41:20 +0100

    [Add] Init Data

---
+++ wdpr_her/backend/api/Controllers/InitDatabaseData.cs
@@ -0,0 +1,376 @@
+﻿using api.DataTemplate;
+using api.Helper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers;
+
+// TODO, doesn't work correctly yet
+public class InitDatabaseData : ControllerBase
+{
+    private readonly StichtingContext _context;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<Gebruiker> _userManager;
+    
+    public InitDatabaseData(StichtingContext context, UserManager<Gebruiker> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+    
+    [HttpPost("InitData")]
+    public async Task<ActionResult> InitData()
+    {
+        #region Roles
+        
+        if (!_roleManager.RoleExistsAsync(Roles.Beheerder).Result)
+        {
+            var role = new IdentityRole();
+            role.Name = Roles.Beheerder;
+            _roleManager.CreateAsync(role).Wait();
+        }
+
+        if (!_roleManager.RoleExistsAsync(Roles.Ervaringsdeskundige).Result)
+        {
+            var role = new IdentityRole();
+            role.Name = Roles.Ervaringsdeskundige;
+            _roleManager.CreateAsync(role).Wait();
+        }
+
+        if (!_roleManager.RoleExistsAsync(Roles.Bedrijf).Result)
+        {
+            var role = new IdentityRole();
+            role.Name = Roles.Bedrijf;
+            _roleManager.CreateAsync(role).Wait();
+        }
+
+        #endregion
+
+        #region Enums
+
+        foreach (var v in setAandoeningen())
+        {
+            var exists = await _context.Aandoeningen.AnyAsync(a => a.Naam.ToLower() == v.Naam.ToLower());
+            if (!exists)
+            {
+                // TODO push first
+            }
+        }
+
+        #endregion
+
+        #region Ervaringsdeskundigen
+        
+        foreach (var e in setDataErvaringsdeskundiges())
+        {
+            Console.WriteLine("Start Deskundige");
+            var userExists = await _userManager.FindByNameAsync(e.Gebruikersnaam);
+            if (userExists == null)
+            {
+                var deskundige = new Ervaringsdeskundige
+                {
+                    Email = e.Email,
+                    Voornaam = e.Voornaam,
+                    Achternaam = e.Acternaam,
+                    Postcode = e.Postcode,
+                    UserName = e.Gebruikersnaam,
+                    PhoneNumber = e.Telefoonnummer,
+                    AccountType = Roles.Ervaringsdeskundige
+                    //Nog meer data over beperkingen en dergelijke, volgt later als dit allemaal werk
+                };
+
+                _userManager.CreateAsync(deskundige, e.Wachtwoord).Wait();
+                _userManager.AddToRoleAsync(deskundige, Roles.Ervaringsdeskundige).Wait();
+                Console.WriteLine("End Deskundige");
+            }
+        }
+        #endregion
+        
+        #region Beheerders
+        
+        foreach (var b in setDataBeheerder())
+        {
+            Console.WriteLine(
+                $"Start Beheerder\n{b.Voornaam}, {b.Acternaam}, {b.Gebruikersnaam}, {b.Email}, {b.Telefoonnummer}, \n");
+            var userExists = await _userManager.FindByNameAsync(b.Gebruikersnaam);
+            if (userExists == null)
+            {
+                var beheerder = new Beheerder
+                {
+                    Email = b.Email,
+                    Voornaam = b.Voornaam,
+                    Achternaam = b.Acternaam,
+                    UserName = b.Gebruikersnaam,
+                    PhoneNumber = b.Telefoonnummer,
+                    AccountType = Roles.Beheerder
+                };
+                Console.WriteLine(
+                    $"Beheerder registreer, gewoon\n{b.Voornaam}, {b.Acternaam}, {b.Gebruikersnaam}, {b.Email}, {b.Telefoonnummer}\n{beheerder.Voornaam},{beheerder.Achternaam}, {beheerder.UserName},{beheerder.Email},{beheerder.PhoneNumber},\n\n");
+
+                _userManager.CreateAsync(beheerder, b.Wachtwoord).Wait();
+                _userManager.AddToRoleAsync(beheerder, Roles.Beheerder).Wait();
+                Console.WriteLine("End Beheerder");
+            }
+        }
+
+        #endregion
+        
+        #region Bedrijven
+
+        foreach (var b in setDataBedrijf())
+        {
+            Console.WriteLine("Start Bedrijf");
+            var userExists = await _userManager.FindByNameAsync(b.Gebruikersnaam);
+            if (userExists == null)
+            {
+                var bedrijf = new Bedrijf
+                {
+                    Email = b.Email,
+                    Kvk = b.Kvk,
+                    Locatie = b.Locatie,
+                    Naam = b.Bedrijfsnaam,
+                    Website = b.Website,
+                    UserName = b.Gebruikersnaam,
+                    PhoneNumber = b.Telefoonnummer,
+                    AccountType = Roles.Bedrijf
+                };
+                _userManager.CreateAsync(bedrijf, b.Wachtwoord).Wait();
+                _userManager.AddToRoleAsync(bedrijf, Roles.Bedrijf).Wait();
+
+                Console.WriteLine("End Bedrijf");
+            }
+        }
+
+        #endregion
+        
+        return Ok();
+    }
+
+    #region Data
+
+    private List<DTORegistreerBeheerder> setDataBeheerder()
+    {
+        var rnd = new Random();
+        var telefoonnummer = 123456789;
+        var ww = "Wachtwoord1!";
+        var emailNum = 47 + 100;
+        return new List<DTORegistreerBeheerder>
+        {
+            new()
+            {
+                Voornaam = "Jack",
+                Acternaam = "Black",
+                Gebruikersnaam = "Fenix",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"beheer{emailNum++}@mail.com"
+            },
+            new()
+            {
+                Voornaam = "Ed",
+                Acternaam = "Max",
+                Gebruikersnaam = "Admin",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"beheer{emailNum++}@mail.com"
+            },
+            new()
+            {
+                Voornaam = "Maria",
+                Acternaam = "Franz",
+                Gebruikersnaam = "Heilung",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"beheer{emailNum++}@mail.com"
+            }
+        };
+    }
+    
+    private List<DTORegistreerBedrijf> setDataBedrijf()
+    {
+        var rnd = new Random();
+        var telefoonnummer = 123456789;
+        var ww = "Wachtwoord1!";
+        var emailNum = 47;
+        return new List<DTORegistreerBedrijf>
+        {
+            new DTORegistreerBedrijf()
+            {
+                Bedrijfsnaam = "Continental",
+                Gebruikersnaam = "JWick",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"bedrijf{emailNum++}@mail.com",
+                Kvk = rnd.NextInt64(10000000, 99999999),
+                Website = "Continental.com",
+                Locatie =
+                    $"{rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}, {rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}"
+            },
+            new DTORegistreerBedrijf()
+            {
+                Bedrijfsnaam = "Yagami",
+                Gebruikersnaam = "Light",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"bedrijf{emailNum++}@mail.com",
+                Kvk = rnd.NextInt64(10000000, 99999999),
+                Website = "Kira.com",
+                Locatie =
+                    $"{rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}, {rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}"
+            },
+            new DTORegistreerBedrijf()
+            {
+                Bedrijfsnaam = "Halloween Town",
+                Gebruikersnaam = "Jack",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"bedrijf{emailNum++}@mail.com",
+                Kvk = rnd.NextInt64(10000000, 99999999),
+                Website = "the_nightmare_before.com",
+                Locatie =
+                    $"{rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}, {rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}"
+            },
+            new DTORegistreerBedrijf()
+            {
+                Bedrijfsnaam = "Feel Good Inc",
+                Gebruikersnaam = "Gorillaz",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"bedrijf{emailNum++}@mail.com",
+                Kvk = rnd.NextInt64(10000000, 99999999),
+                Website = "Cracker_Island.com",
+                Locatie =
+                    $"{rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}, {rnd.NextInt64(-60, 60)}.{rnd.NextInt64(99999)}"
+            }
+        };
+    }
+
+    private List<DTORegistreerErvaringsdeskundige> setDataErvaringsdeskundiges()
+    {
+        var rnd = new Random();
+        var telefoonnummer = 123456789;
+        var ww = "Wachtwoord1!";
+        var emailNum = 47;
+        var deskundigenLijst = new List<DTORegistreerErvaringsdeskundige>
+        {
+            new()
+            {
+                Voornaam = "Steven",
+                Acternaam = "Wilson",
+                Gebruikersnaam = "Voyage34",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "Jack",
+                Acternaam = "White",
+                Gebruikersnaam = "Raconteurs",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "Maynard",
+                Acternaam = "Keenan",
+                Gebruikersnaam = "A_Perfect_Circle",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "John",
+                Acternaam = "Entwistle",
+                Gebruikersnaam = "ThunderFingers",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "Cliff",
+                Acternaam = "Burton",
+                Gebruikersnaam = "RIP",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "Jeff",
+                Acternaam = "Wayne",
+                Gebruikersnaam = "War_of_the_worlds",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "Till",
+                Acternaam = "Lindemann",
+                Gebruikersnaam = "Rammstein",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            },
+            new()
+            {
+                Voornaam = "Franck",
+                Acternaam = "Hueso",
+                Gebruikersnaam = "Carpenter Brut",
+                Wachtwoord = ww,
+                Telefoonnummer = $"+{rnd.NextInt64(99)}-{telefoonnummer + rnd.NextInt64(999999999)}",
+                Email = $"mail_ed{++emailNum}@mail.com",
+                Postcode = $"{rnd.NextInt64(1000, 9999)} AB"
+            }
+        };
+        return deskundigenLijst;
+    }
+
+    private List<DTOHelper> setAandoeningen()
+    {
+        return new List<DTOHelper>()
+        {
+            new DTOHelper() { Naam = "Slechtziend" },
+            new DTOHelper() { Naam = "Blind" },
+            new DTOHelper() { Naam = "Dyslectisch" },
+            new DTOHelper() { Naam = "Slechthorend" },
+            new DTOHelper() { Naam = "Doof" }
+        };
+    }
+    
+    private List<DTOHelper> setBenadering()
+    {
+        return new List<DTOHelper>()
+        {
+            new DTOHelper() { Naam = "E-mail" },
+            new DTOHelper() { Naam = "Telefonisch" },
+            new DTOHelper() { Naam = "Chat" }
+        };
+    }
+    
+    private List<DTOHelper> setOnderzoeksTypes()
+    {
+        return new List<DTOHelper>()
+        {
+            new DTOHelper() { Naam = "Enquête" },
+            new DTOHelper() { Naam = "Interview" },
+            new DTOHelper() { Naam = "Op Locatie" },
+            new DTOHelper() { Naam = "Engelstalig" },
+        };
+    }
+
+    #endregion
+}
