fab1ef4794917a2b0a975e1a54b160cab48535e6
BarmanTurbo <21035407@student.hhs.nl>
2024-01-10 18:07:35 +0100

    documentatie DynamicOpdrachtPaneel

--- wdpr_her/src/standaardformats/DynamicOpdrachtPaneel.js
+++ wdpr_her/src/standaardformats/DynamicOpdrachtPaneel.js
@@ -6,6 +6,17 @@ import OpdrachtPaneel from "../standaardformats/OpdrachtPaneel";
 const DynamicOpdrachtPaneel = ({ opdrachtArray }) => {
   const [opdrachtCounter, setOpdrachtCounter] = useState(1);
 
+  /*
+  Hoe werkt DynamicOpdrachtPaneel?
+  Je geeft een Array mee met Objecten die allemaal elk kunnen fucntioneren als OpdrachtPaneel.
+  const opdrachtArray = [
+        { naam: "If it hadn't been for", aantaldeelnemers: 69 }];
+        return(
+          <DynamicOpdrachtPaneel opdrachtArray={opdrachtArray} />
+        )
+        Dan maakt die x Opdrachtpanelen onder elkaar, waar X het aantal objecten in opdrachtArray is.
+        Ze worden allemaal automatisch geindexeerd.
+  */
   const handleAddOpdracht = () => {
     setOpdrachtCounter((prevCounter) => prevCounter + 1);
   };
