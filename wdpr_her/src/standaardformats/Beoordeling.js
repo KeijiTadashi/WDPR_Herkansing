b362e0bd101778ab09d4b01fb650c5fa1460b6fb
BarmanTurbo <21035407@student.hhs.nl>
2024-01-12 10:16:36 +0100

    fixed accessibility Beoordeling

--- wdpr_her/src/standaardformats/Beoordeling.js
+++ wdpr_her/src/standaardformats/Beoordeling.js
@@ -1,6 +1,6 @@
 // Beoordeling.js
 import React from 'react';
-import '../CSS/StichtingTheme'
+import '../CSS/StichtingTheme.css'
 import useLocalStorage from 'use-local-storage';
 
 function Beoordeling() {
@@ -11,27 +11,54 @@ function Beoordeling() {
     const [fontSize] = useLocalStorage('font-size', 'normal');
 
     return (
-        <div className="Beoordeling">
-            <label for='checkbox_zeer_goed'>
-                <input type="checkbox" value="1" name='checkbox_zeer_goed'/>
-                Zeer goed
-            </label>
-            <label>
-                <input type="checkbox" value="2" />
-                Goed
-            </label>
-            <label>
-                <input type="checkbox" value="3" />
-                Neutraal
-            </label>
-            <label>
-                <input type="checkbox" value="4" />
-                Slecht
-            </label>
-            <label>
-                <input type="checkbox" value="5" />
-                Zeer slecht
-            </label>
+        <div className="Main" data-theme={theme} data-font-size={fontSize}>
+            <div className="Beoordeling">
+                
+                <label for='checkbox_zeer_goed'>
+                    <input
+                        type="checkbox"
+                        value="1"
+                        name='checkbox_zeer_goed'
+                    />
+                    Zeer goed
+                </label>
+                
+
+                <label for='checkbox_goed'>
+                    <input 
+                        type="checkbox"
+                        value="2"
+                        name='checkbox_goed' 
+                    />
+                    Goed
+                </label>
+
+                <label for='checkbox_neutraal'>
+                    <input
+                        type="checkbox"
+                        value="3"
+                        name='checkbox_neutraal'
+                    />
+                    Neutraal
+                </label>
+
+                <label for='checkbox_slecht'>
+                    <input
+                        type="checkbox"
+                        value="4"
+                        name='checkbox_slecht'
+                    />
+                    Slecht
+                </label>
+                <label for='checkbox_zeer_slecht'>
+                    <input
+                        type="checkbox"
+                        value="5"
+                        name='checkbox_zeer_slecht'
+                    />
+                    Zeer slecht
+                </label>
+            </div>
         </div>
     );
 }
