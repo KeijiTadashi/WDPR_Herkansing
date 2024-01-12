00e02d4a38bc058da544ae8ed005a220a521c841
BarmanTurbo <21035407@student.hhs.nl>
2024-01-12 11:55:16 +0100

    begone

--- wdpr_her/src/standaardformats/DynamicOnderzoekPaneel.js
+++ wdpr_her/src/standaardformats/DynamicOnderzoekPaneel.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import OnderzoekItem from '../standaardformats/OnderzoekItem';
 
 const DynamicOnderzoekPaneel = ({ onderzoekArray }) => {
