0e43da9a1fd6329ece48a95c5986b13377d7c377
BarmanTurbo <21035407@student.hhs.nl>
2024-01-12 09:50:59 +0100

    fix fontsize Login.js

--- wdpr_her/src/Pages/Login.js
+++ wdpr_her/src/Pages/Login.js
@@ -1,60 +1,69 @@
 import Header from "../standaardformats/Header";
-import {apiPath} from "../Helper/Api";
+import { apiPath } from "../Helper/Api";
 import axios from "axios";
 // import {Layout} from "../standaardformats/Layout";
-import {useRef} from "react";
-import {SetAuthToken} from "../Helper/AuthToken";
+import { useRef } from "react";
+import { SetAuthToken } from "../Helper/AuthToken";
+
+import useLocalStorage from "use-local-storage";
 
 
 export function Login() {
     const usernameRef = useRef(null);
     const passwordRef = useRef(null);
 
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    const [theme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+    const [fontSize] = useLocalStorage('font-size', 'normal');
+
     return (
         // <Layout>
         <>
-        <Header/>
-            <form>
-                <label for="UsernameField">Gebruikersnaam</label>
-                <br/>
-                <input 
-                    type={"text"}
-                    id={"username"}
-                    ref={usernameRef}
-                    name="UsernameField"
-                    aria-label="Invoerveld gebruikersnaam"
-                >
-                </input>
+            <Header />
+            <div className="Main" data-theme={theme} data-font-size={fontSize}>
+                <form>
+                    <label for="UsernameField">Gebruikersnaam</label>
+                    <br />
+                    <input
+                        type={"text"}
+                        id={"username"}
+                        ref={usernameRef}
+                        name="UsernameField"
+                        aria-label="Invoerveld gebruikersnaam"
+                    >
+                    </input>
 
-                <br/>
+                    <br />
 
-                <label for="PasswordField">Wachtwoord</label>
-                <br/>
-                <input
-                    type={"password"}
-                    id={"password"}
-                    ref={passwordRef}
-                    name="PasswordField"
-                    aria-label="Invoerveld wachtwoord"
-                >
-                </input>
-                <br/>
+                    <label for="PasswordField">Wachtwoord</label>
+                    <br />
+                    <input
+                        type={"password"}
+                        id={"password"}
+                        ref={passwordRef}
+                        name="PasswordField"
+                        aria-label="Invoerveld wachtwoord"
+                    >
+                    </input>
+                    <br />
 
-                <button
-                    aria-label="Log in"
-                    type="button"
-                    onClick={() =>
-                        LoginJWT(
-                            usernameRef.current.value,
-                            passwordRef.current.value
-                        )
-                    }
-                >
-                    Log in
-                </button>
-            </form>
+                    <button
+                        aria-label="Log in"
+                        type="button"
+                        onClick={() =>
+                            LoginJWT(
+                                usernameRef.current.value,
+                                passwordRef.current.value
+                            )
+                        }
+                    >
+                        Log in
+                    </button>
+                </form>
+            </div>
         </>
-         // </Layout>
+        // </Layout>
     )
 }
 
