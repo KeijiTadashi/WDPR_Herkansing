d039cc9a3af6ed433e7718b48b32c80f38f61e23
Brian <17700667+KeijiTadashi@users.noreply.github.com>
2024-01-12 11:43:59 +0100

    Swapped ApiPath Comment

--- wdpr_her/src/Helper/Api.js
+++ wdpr_her/src/Helper/Api.js
@@ -1,5 +1,5 @@
 import {isDev} from "./Development";
 
 // Todo Change the else (second link) to the online API path once it's online
-// export const apiPath = isDev() ? "http://localhost:5027/" : "https://apiwdprher.azurewebsites.net/";
-export const apiPath = isDev() ? "https://apiwdprher.azurewebsites.net/" : "https://apiwdprher.azurewebsites.net/";
+export const apiPath = isDev() ? "http://localhost:5027/" : "https://apiwdprher.azurewebsites.net/";
+// export const apiPath = isDev() ? "https://apiwdprher.azurewebsites.net/" : "https://apiwdprher.azurewebsites.net/";
