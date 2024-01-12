54d8cc4c7e79a43de8c9bc0066f696ec9beb6a6f
EmreDH <20150393@student.hhs.nl>
2024-01-12 10:54:37 +0100

    bedrijf API
    api

--- wdpr_her/backend/api/Controllers/BedrijfController.cs
+++ wdpr_her/backend/api/Controllers/BedrijfController.cs
@@ -1,6 +1,26 @@
-﻿namespace api.Controllers;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class BedrijfController
+namespace api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class BedrijfController : ControllerBase
 {
-    
+
+    private readonly StichtingContext _context;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<Gebruiker> _userManager;
+    public BedrijfController(UserManager<Gebruiker> userManager, RoleManager<IdentityRole> roleManager, StichtingContext context)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _context = context;
+    }
+
+    [HttpGet("GetAllBedrijven")]
+    public async Task<ActionResult<IEnumerable<Bedrijf>>> GetAllBedrijven()
+    {
+        return await _context.Bedrijven.ToListAsync();
+    }
 }
